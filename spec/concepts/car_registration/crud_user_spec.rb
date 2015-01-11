46d8efd3c073684edc4f22925811fe846a2e8ce1
Thomas Baustert <business@thomasbaustert.de>
2015-01-11 17:50:18 +0100

    update to reform 1.2.5, rspec 3

--- spec/concepts/car_registration/crud_user_spec.rb
+++ spec/concepts/car_registration/crud_user_spec.rb
@@ -8,15 +8,15 @@ describe "Crud by user role" do
     it "persists valid" do
       car_registration = CarRegistration::Create[current_user: user, car_registration: { number_plate: "HH AB 100" }].model
 
-      expect(car_registration.persisted?).to be_true
+      expect(car_registration.persisted?).to be true
       expect(car_registration.number_plate).to eq "HH AB 100"
     end
 
     it "invalid" do
       res, op = CarRegistration::Create.run(current_user: user, car_registration: { number_plate: "" })
 
-      expect(res).to be_false
-      expect(op.model.persisted?).to be_false
+      expect(res).to be false
+      expect(op.model.persisted?).to be false
       expect(op.contract.errors.to_s).to eq "{:number_plate=>[\"can't be blank\"]}"
     end
 
@@ -25,8 +25,8 @@ describe "Crud by user role" do
       res, op = CarRegistration::Create.run(current_user: user,
                                             car_registration: { number_plate: "HH AB 100", chassis_number: "ABC" })
 
-      expect(res).to be_false
-      expect(op.model.persisted?).to be_false
+      expect(res).to be false
+      expect(op.model.persisted?).to be false
       expect(op.contract.errors.to_s).to eq "{:chassis_number=>[\"is too short (minimum is\ 4 characters)\"]}"
     end
   end
@@ -62,7 +62,7 @@ describe "Crud by user role" do
       # A: We don't have to pass it cause we have a own contract per role!
       # operation.validate(current_user: user, car_registration: { number_plate: "HH AB 100", chassis_number: "ABCD1234" })
       result = operation.validate(number_plate: "HH AB 100", chassis_number: "ABCD1234")
-      expect(result).to be_true
+      expect(operation.valid?).to be true
     end
   end
 
